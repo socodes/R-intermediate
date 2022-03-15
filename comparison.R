351645a4bf4d58be99e90a464256ad4df7857881
Muhammed Didin <mdidin80@gmail.com>
2022-03-15 22:54:42 +0300

    basic comparison operations

---
+++ comparison.R
@@ -0,0 +1,14 @@
+# Comparison of logicals
+TRUE == FALSE
+TRUE > FALSE #returns TRUE
+
+# Comparison of numerics
+-6*14 != 17-101
+(-6 * 5 + 2) >= -10 + 1
+
+# Comparison of strings
+"userR" == "user"
+"raining" <= "rainingDogs"
+
+# Compare a logical with a numeric
+TRUE == 1 # returns TRUE
